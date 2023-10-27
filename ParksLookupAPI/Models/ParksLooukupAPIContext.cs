8a182bda4338cca0dda4f0ad93851a14c992d99e
onurkaymak <onurkaymak@outlook.com>
2023-10-27 09:18:15 -0700

    Seed the database.

--- ParksLookupAPI/Models/ParksLooukupAPIContext.cs
+++ ParksLookupAPI/Models/ParksLooukupAPIContext.cs
@@ -12,7 +12,26 @@ namespace ParksLookupAPI.Models
 
     public ParksLookupAPIContext(DbContextOptions<ParksLookupAPIContext> options) : base(options)
     {
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<State>()
+        .HasData(
+          new State { StateId = 1, Name = "Colorado" }
+        );
 
+      builder.Entity<Park>()
+        .HasData(
+          new Park { ParkId = 1, Name = "Rocky Mountain National Park", StateId = 1, UserId = "onr" }
+        );
+
+      var hasher = new PasswordHasher<IdentityUser>();
+
+      builder.Entity<ApplicationUser>()
+        .HasData(
+          new ApplicationUser { Id = "onr", UserName = "Onur", PasswordHash = hasher.HashPassword(null, "@Test123"), Email = "onur@test.com", NormalizedEmail = "ONUR@TEST.COM" }
+        );
+      base.OnModelCreating(builder);
     }
   }
 }
