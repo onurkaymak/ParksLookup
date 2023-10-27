cfc2d4215b20f675cb2e59619bc6892eeab5d869
onurkaymak <onurkaymak@outlook.com>
2023-10-27 10:09:03 -0700

    Add DateTime migration to update Parks Entity.

--- ParksLookupAPI/Migrations/20231027170703_AddDateTimeForPark.Designer.cs
+++ ParksLookupAPI/Migrations/20231027170703_AddDateTimeForPark.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ParksLookupAPI.Models;
 
@@ -10,9 +11,10 @@ using ParksLookupAPI.Models;
 namespace ParksLookupAPI.Migrations
 {
     [DbContext(typeof(ParksLookupAPIContext))]
-    partial class ParksLookupAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20231027170703_AddDateTimeForPark")]
+    partial class AddDateTimeForPark
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ParksLookupAPI/Migrations/20231027170703_AddDateTimeForPark.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParksLookupAPI.Migrations
+{
+    public partial class AddDateTimeForPark : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EstablishedAsPark",
+                table: "Parks",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "onr",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "c7345f06-5ffd-45a3-a9f4-65c19e88d865", "AQAAAAEAACcQAAAAEJsqAUYMuC2CLkUmFwdpeweGIlpCjG6vfrEtOpghn1zahN//dDE4EBRmEL8qRBp60w==", "5d6c3e52-4a42-4d56-bd3e-8db4115f9c1c" });
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1,
+                column: "EstablishedAsPark",
+                value: new DateTime(1915, 1, 26, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2,
+                column: "EstablishedAsPark",
+                value: new DateTime(1968, 10, 2, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3,
+                column: "EstablishedAsPark",
+                value: new DateTime(1899, 3, 2, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4,
+                column: "EstablishedAsPark",
+                value: new DateTime(1902, 5, 22, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstablishedAsPark",
+                table: "Parks");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "onr",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "34f82fb9-c37b-47e7-8bc7-c886e916bbe6", "AQAAAAEAACcQAAAAEFd1YnCR3t9/HUkNC4MYkD+tcQH7TNlxK3zBNMgF3sV/w4QtMjZZAHIgqX4+4OJRGQ==", "d8ed9565-4870-4dda-a3e2-d188fb777b8f" });
+        }
+    }
+}
